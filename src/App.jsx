7d3733e299121159ec5a384e8328ac386fa7081e
SirioRedGiant <Oiris1992@gmail.com>
2026-02-18 14:59:35 +0100

    Add react-router-dom and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 export default function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index Component={HomePage} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
