5e112799da815089edc421a220d85955f51073c7
SirioRedGiant <Oiris1992@gmail.com>
2026-02-19 16:55:09 +0100

    Add: modify product data handling in ProductsPage to shorten titles for display

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -4,14 +4,32 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 export default function ProductsPage() {
-  const [data, setData] = useState([]);
+  const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get("https://fakestoreapi.com/products")
       .then((response) => {
-        setData(response.data);
+        //note --> MANIPOLAZIONE DEI DATI: dati modificati partendo da response.data
+        const productsData = response.data.map((prod) => {
+          //response data è l'array orriginale di 20 prodotti --> il metodo .map() crea un nuovo array. Per ogni prodotto (prod) esegue una funzione e salva il risultato nel nuovo array --> ProductsData, questo perchè non si vuole modificare l'originale, ma creare una versione "personalizzata" da utilizzare per le card
+
+          const maxLength = 26; // Se il titolo è lungo, prendiamo gli ultimi 26 caratteri
+
+          const shortTitle =
+            prod.title.length > maxLength
+              ? "..." + prod.title.substring(prod.title.length - maxLength)
+              : prod.title;
+
+          return {
+            ...prod, // Lo "Spread Operator" copia tutte le proprietà originali (id, title, price, image, ecc.)
+            title: shortTitle, // Sovrascrive solo la proprietà title con la versione corta
+          };
+        });
+
+        setProducts(productsData); // alla fine si passa questo nuovo array "pulito" allo stato di React. Quando React vede il nuovo stato, avvia il re-render e mostra i titoli accorciati nelle card.
+
         setIsLoading(false);
       })
       .catch((error) => {
@@ -28,7 +46,7 @@ export default function ProductsPage() {
     <div className="container py-5">
       <h1 className="text-center mb-4">Our Products</h1>
       <div className="row g-4">
-        {data.map((product) => (
+        {products.map((product) => (
           <div key={product.id} className="col-12 col-md-6 col-lg-4 col-xl-3">
             <div className="card h-100 p-3 shadow-sm text-center">
               <img
